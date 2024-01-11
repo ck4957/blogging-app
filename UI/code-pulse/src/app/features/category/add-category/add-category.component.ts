2f3a6e04ff63c049a1d4e4cdec39a5d6549fcf0b
Chirag Kular <kular_chirag@yahoo.com>
2024-01-11 22:34:44 +0000

    Add Category components

---
+++ UI/code-pulse/src/app/features/category/add-category/add-category.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnDestroy } from '@angular/core';
+import { AddCategoryRequest } from '../models/add-category-request.model';
+import { CategoryService } from '../services/category.service';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-add-category',
+  templateUrl: './add-category.component.html',
+  styleUrls: ['./add-category.component.css']
+})
+export class AddCategoryComponent implements OnDestroy {
+
+  model: AddCategoryRequest;
+  private addCategorySubscription?: Subscription;
+
+  constructor(private categoryService: CategoryService) {
+    this.model = {
+      name: '',
+      urlHandle: ''
+    }
+  }
+
+  onFormSubmit() {
+    this.addCategorySubscription =  this.categoryService.addCategory(this.model).subscribe({
+      next: (response) => {
+
+      }
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.addCategorySubscription?.unsubscribe();
+  }
+
+}
