627fff2bd13b115bb0a8e8879c3692c6d2f8814f
Chirag Kular <kular_chirag@yahoo.com>
2024-01-11 20:10:01 +0000

    Create Post method for adding categories

---
+++ API/CodePulse.API/Controllers/CategoriesController.cs
@@ -0,0 +1,44 @@
+﻿using CodePulse.API.Data;
+using CodePulse.API.Models.Domain;
+using CodePulse.API.Models.DTO;
+using CodePulse.API.Repositories.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodePulse.API.Controllers
+{
+    // https://localhost:xxxx/api/categories
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        public CategoriesController(ICategoryRepository categoryRepository) { 
+            this._categoryRepository = categoryRepository;
+        }
+        
+        // 
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryRequestDto requestDto)
+        {
+            // Map DTO to Domain Model
+            var category = new Category
+            {
+                Name = requestDto.Name,
+                UrlHandle = requestDto.UrlHandle
+            };
+
+            await _categoryRepository.CreateAsync(category);
+           
+            // Domain Model to DTO
+            var response = new CategoryDto
+            {
+                Id = category.Id,
+                Name = category.Name,
+                UrlHandle = category.UrlHandle
+            };
+
+            return Ok(response);
+        }
+    }
+}
