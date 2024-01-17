ea3209a209c042410c5fa717c011da6280d6082c
Chirag Kular <kular_chirag@yahoo.com>
2024-01-17 19:08:33 +0000

    Adding JwtDecode

--- UI/code-pulse/src/app/features/auth/guards/auth.guard.ts
+++ UI/code-pulse/src/app/features/auth/guards/auth.guard.ts
@@ -1,5 +1,41 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+import { AuthService } from '../services/auth.service';
+import { jwtDecode } from "jwt-decode";
 
 export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+  const cookieService = inject(CookieService);
+  const authService = inject(AuthService);
+  const router = inject(Router);
+  const user = authService.getUser();
+
+  let token = cookieService.get('Authorization');
+  if (token && user) {
+    token = token.replace('Bearer', '');
+    const decodedToken: any = jwtDecode(token);
+
+    // Check if token has expired
+    const expirationDate = decodedToken.exp * 1000;
+    const currentTime = new Date().getTime();
+
+    if (expirationDate < currentTime) {
+      // Logout
+      authService.logout();
+      return router.createUrlTree(['/login'], { queryParams: {returnUrl: state.url}})
+
+    } else {
+      // token is still valid
+      if (user.roles.includes('writer')) {
+        return true;
+      } else {
+        alert('Unauthorized');
+        return false;
+
+      }
+    }
+  } else {
+    authService.logout();
+    return router.createUrlTree(['/login'], { queryParams: { returnUrl: state.url } })
+  }
 };
