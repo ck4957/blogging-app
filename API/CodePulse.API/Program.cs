77bf99b02d4f337cb27ea7c0268f9e634ef1613c
Chirag Kular <kular_chirag@yahoo.com>
2024-01-17 14:59:48 +0000

    API - Image Selector

--- API/CodePulse.API/Program.cs
+++ API/CodePulse.API/Program.cs
@@ -2,12 +2,14 @@ using CodePulse.API.Data;
 using CodePulse.API.Repositories.Implementation;
 using CodePulse.API.Repositories.Interface;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileProviders;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddHttpContextAccessor();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -18,6 +20,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<IBlogPostRepository, BlogPostRepository>();
+builder.Services.AddScoped<IImageRepository, ImageRepository>();
 
 var app = builder.Build();
 
@@ -37,6 +40,13 @@ app.UseCors(options =>
     options.AllowAnyMethod();
 });
 app.UseAuthorization();
+
+// Allow Static Files/Images to be seen
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "Images")),
+    RequestPath = "/Images"
+});
 
 app.MapControllers();
 
---
+++ API/CodePulse.API/Repositories/Implementation/ImageRepository.cs
@@ -0,0 +1,46 @@
+﻿using CodePulse.API.Data;
+using CodePulse.API.Models.Domain;
+using CodePulse.API.Repositories.Interface;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodePulse.API.Repositories.Implementation
+{
+    public class ImageRepository : IImageRepository
+    {
+        private readonly ApplicationDbContext dbContext;
+        private readonly IWebHostEnvironment webHostEnvironment;
+        private readonly IHttpContextAccessor httpContextAccessor;
+        public ImageRepository(IWebHostEnvironment webHostEnvironment, 
+            ApplicationDbContext dbContext,
+            IHttpContextAccessor httpContextAccessor)
+        {
+            this.webHostEnvironment = webHostEnvironment;
+            this.dbContext = dbContext;
+            this.httpContextAccessor = httpContextAccessor;
+        }
+
+        public async Task<IEnumerable<BlogImage>> GetAll()
+        {
+            return await dbContext.BlogImages.ToListAsync();
+        }
+
+        public async Task<BlogImage> Upload(IFormFile file, BlogImage blogImage)
+        {
+            // 1. Upload Image to API/Image
+            var localPath = Path.Combine(webHostEnvironment.ContentRootPath, "Images", $"{blogImage.FileName}{blogImage.FileExtension}");
+            using var stream = new FileStream(localPath, FileMode.Create);
+            await file.CopyToAsync(stream);
+
+            // 2. Update DB
+            var httpRequest = httpContextAccessor.HttpContext.Request;
+            var urlPath = $"{httpRequest.Scheme}://{httpRequest.Host}{httpRequest.PathBase}/Images/{blogImage.FileName}{blogImage.FileExtension}";
+
+            blogImage.Url = urlPath;
+
+            await dbContext.BlogImages.AddAsync(blogImage);
+            await dbContext.SaveChangesAsync();
+
+            return blogImage;
+        }
+    }
+}
